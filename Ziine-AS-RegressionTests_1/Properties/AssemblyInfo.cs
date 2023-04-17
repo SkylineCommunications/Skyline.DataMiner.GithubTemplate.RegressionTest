e5fdf0b64d9ba7f0edee1d138a78f6242783b586
Jarno Lernou <jarno.lernou@skyline.be>
2023-04-17 22:29:03 +0200

    rename

---
+++ Ziine-AS-RegressionTests_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Ziine-AS-RegressionTests_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Ziine-AS-RegressionTests_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("16BCEA61-E88F-483C-A643-4718994074A4")]
+[assembly: AssemblyVersion("1.0.0.0")]
