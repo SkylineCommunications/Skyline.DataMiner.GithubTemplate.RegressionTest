562bd68fc8fa9799cb83a50b36268dde930280f5
Jarno Lernou <114563645+jarno-lernou@users.noreply.github.com>
2023-05-23 14:31:25 +0200

    Update TestInfoConsts.cs
    Added extra information on what the Contact should be

--- Library/Consts/TestInfoConsts.cs
+++ Library/Consts/TestInfoConsts.cs
@@ -7,7 +7,7 @@
 
 	internal static class TestInfoConsts
 	{
-		internal static string Contact => "@skyline.be"; // TODO: Change contact person
+		internal static string Contact => "@skyline.be"; // TODO: The 'Contact' field must be your squad or domain e-mail. No Individual e-mails are allowed.
 
 		internal static List<int> ProjectIds => new List<int> { -1 }; // TODO: Change project ID
 	}
